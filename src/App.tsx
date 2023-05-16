be1d8db83f1e53f62627cedf3ed495c76e5a9235
eric-yates <eric@medleyagency.com>
2023-05-16 14:12:41 -0700

    Add demo webcam capture

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import WebcamStreamCapture from './WebcamStreamCapture.js';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to our meditation app!
-        </p>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            {/*<header className="App-header">*/}
+            {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+            {/*  <p>*/}
+            {/*    Welcome to our meditation app!*/}
+            {/*  </p>*/}
+            {/*</header>*/}
+            <WebcamStreamCapture/>
+        </div>
+    );
 }
 
 export default App;
